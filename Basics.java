d0a1bc829c6b51b45a34e523573d78d45d9eb099
Ankit jhall <47866823+ankitjhall@users.noreply.github.com>
2021-10-06 21:00:45 -0400

    Add files via upload

---
+++ Basics.java
@@ -0,0 +1,23 @@
+
+public class Basics {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+System.out.println("I navigated to home page");
+
+// how to call the methods present in other class to this class
+
+// for that you need to create an object first.
+
+// we need to tell basics that there is a class present like basics_2
+
+Basics_2 m = new Basics_2();
+
+m.ValidateHeader();
+
+//classobject.methodname	
+		
+	}
+
+}
